605a4c30a6cb2130f6a9fa6e1ea973d31d4344fc
Seth Schallau <sethmschallau@gmail.com>
2023-12-07 22:47:14 -0500

    Day one part one

--- code/day1.py
+++ code/day1.py
@@ -1,7 +1,15 @@
-def main():
-    with open('input/day1.txt', 'r') as file:
-        data = file.read()
-        print(data)
+import re
 
+def main():
+    total = 0
+    with open('input/day1.txt', 'r') as file:
+        for line in file:
+            numbers = re.sub(r'\D', '', line)
+
+            if(len(numbers) == 1):
+                numbers += numbers[0]
+                
+            total += (int(numbers[0]) * 10) + int(numbers[-1])
+    print('Final: ' + str(total) + '\n')
 if __name__ == '__main__':
     main()
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if exactly one argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <number>"
     exit 1
@@ -15,8 +14,8 @@ touch "input/day${x}.txt"
 # Create a Python file in the code directory with a structure to read the input file
 echo "def main():
     with open('../input/day${x}.txt', 'r') as file:
-        data = file.read()
-        # Process the data as needed
+        for line in file:
+            print(line.strip())
 
 if __name__ == '__main__':
     main()" > "code/day${x}.py"
