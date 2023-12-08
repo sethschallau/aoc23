f89aad6242a682c74fae9b4262c0fd8f70314d78
Seth Schallau <sethmschallau@gmail.com>
2023-12-07 23:52:04 -0500

    day one part two

--- code/day1.py
+++ code/day1.py
@@ -2,14 +2,39 @@ import re
 
 def main():
     total = 0
+    mapping = {
+    'one': 1, '1': 1,
+    'two': 2, '2': 2,
+    'three': 3, '3': 3,
+    'four': 4, '4': 4,
+    'five': 5, '5': 5,
+    'six': 6, '6': 6,
+    'seven': 7, '7': 7,
+    'eight': 8, '8': 8,
+    'nine': 9, '9': 9
+    }
+
     with open('input/day1.txt', 'r') as file:
         for line in file:
-            numbers = re.sub(r'\D', '', line)
+            left = 0
+            right = 0
+            pointer = 0
+            while(pointer < len(line)):
+                for x in mapping:
+                    if len(x) <= (len(line) - pointer):
+                        if line[pointer:pointer+len(x)] == x:
+                            if left == 0:
+                                left = right = mapping[x]
+                            else:
+                                right = mapping[x]
+                pointer += 1
 
-            if(len(numbers) == 1):
-                numbers += numbers[0]
-                
-            total += (int(numbers[0]) * 10) + int(numbers[-1])
+            
+            total += (left * 10) + right
     print('Final: ' + str(total) + '\n')
 if __name__ == '__main__':
     main()
+'''
+if string is length 4 and I'm on iteration 1 it's 
+
+'''
