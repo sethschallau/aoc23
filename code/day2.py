8714b8ee303aa18889c14d6c1dd5017a6798ae93
Seth Schallau <sethmschallau@gmail.com>
2023-12-10 22:06:42 -0500

    day two part two

--- code/day2.py
+++ code/day2.py
@@ -2,26 +2,30 @@ import re
 
 def main():
     mapping = {
-        'red' : 12,
-        'green' : 13,
-        'blue' : 14
+    'red' : 0,
+    'green' : 1,
+    'blue' : 2
     }
-    bads = set()
-    allofthem = set()
+    total = 0
     with open('input/day2.txt', 'r') as file:
+        max = []
         for line in file:
+            max = [0,0,0]
             id = int(re.search(r'\d+', line).group())
-            allofthem.add(id)
             bags = line.split(':')[1].strip()
             seperated = bags.split(';')
             for x in seperated:
                 hands = x.strip().split(',')
                 for y in hands:
                     y = y.strip().split(' ')
-                    if mapping[y[1]] < int(y[0]):
-                        if(id not in bads):
-                            bads.add(id)
-    print(sum(allofthem - bads))
+                    if max[mapping[y[1]]] < int(y[0]):
+                        max[mapping[y[1]]] = int(y[0])
 
+            start = 1
+            for x in max:
+                start *= x
+
+            total += start
+    print(total)
 if __name__ == '__main__':
     main()
