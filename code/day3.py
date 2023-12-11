0228f43e3a13c5fa379fa1f75d23ab3c60a6af43
Seth Schallau <sethmschallau@gmail.com>
2023-12-11 00:45:57 -0500

    day three part one

---
+++ code/day3.py
@@ -0,0 +1,43 @@
+def main():
+    directions = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]
+    matrix = []
+    matrix.append(['.']*12)
+    with open('input/day3.txt', 'r') as file:
+        for line in file:
+            line = line.strip()
+            temp = []
+            temp.append('.')
+            for x in line:
+                temp.append(x)
+            temp.append('.')
+            matrix.append(temp)
+    matrix.append(['.']*12)
+
+
+    total = 0
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if 48 <= ord(matrix[i][j]) <= 57 or ord(matrix[i][j]) == 46:
+                continue
+            
+            been_there = set()
+            for dx, dy, in directions:
+                temp = []
+                if(48 <= ord(matrix[i+dy][j+dx]) <= 57):
+                    temp.append(matrix[i+dy][j+dx])
+                    left = 1
+                    while(48 <= ord(matrix[i+dy][j+dx-left]) <= 57):
+                        temp.insert(0,matrix[i+dy][j+dx-left])
+                        left += 1
+                    right = 1
+                    while(48 <= ord(matrix[i+dy][j+dx+right]) <= 57):
+                        temp.append(matrix[i+dy][j+dx+right])
+                        right += 1
+                if temp:
+                    value = int(''.join(temp))
+                    if value not in been_there:
+                        been_there.add(value)
+                        total += value
+    print(total)
+if __name__ == '__main__':
+    main()
