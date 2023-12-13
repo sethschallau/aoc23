952667a6459ecdde44b2119f394658e037d422f2
Seth Schallau <seth.schallau@vitalsource.com>
2023-12-12 20:53:51 -0500

    day four part two

--- code/day4.py
+++ code/day4.py
@@ -1,4 +1,7 @@
+from collections import deque
 def main():
+    processed = []
+    index = 0
     total = 0
     with open('input/day4.txt', 'r') as file:
         for line in file:
@@ -9,10 +12,20 @@ def main():
             winners = set(winners.split(' '))
             yours = set(yours.split(' '))
             intersect = len(winners & yours)
-            if intersect != 0:
-                total += 2 ** (intersect-1)
+            processed.append((index, intersect))
+            index += 1
+
+    q = deque()
+    for i, val in processed:
+            total += 1
+            q.appendleft((i,val))
+
+    while q:
+        curr = q.pop()
+        if curr[1] != 0:
+            for card in processed[curr[0]+1:curr[0]+curr[1]+1]:
+                total += 1
+                q.appendleft(card)
     print(total)
-
-
 if __name__ == '__main__':
     main()
